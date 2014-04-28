43d0a7d5dafeeb903b5d78f26040921069bf66b5
renzon <renzon@gmail.com>
2014-04-28 20:17:29 -0300

    timezone com pytz

---
+++ backend/src/web/tz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from datetime import datetime
+from pytz import timezone, utc
+
+sp_timezone = timezone('America/New_York')
+
+
+def index(_json):
+    agora_utc = datetime.now(tz=utc)
+
+    agora_sp = agora_utc.astimezone(sp_timezone)
+    agora_utc_str = agora_utc.strftime('%Y/%m/%d %H:%M:%S')
+    agora_sp_str = agora_sp.strftime('%Y/%m/%d %H:%M:%S')
+    dct = {'utc': agora_utc_str, 'sp': agora_sp_str}
+    _json(dct)
